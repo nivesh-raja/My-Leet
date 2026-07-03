d647c1e3b7c4ac4190eca4bc833dc80e5258c4db
nivesh-raja <rniveshraja@gmail.com>
2026-07-03 12:19:00 +0530

    Time: 2704 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,5 @@
+SELECT t.student_id,t.student_name,t.subject_name,COUNT(e.student_id) AS attended_exams
+FROM Examinations e RIGHT JOIN(SELECT * FROM Students s JOIN Subjects)t
+ON e.student_id=t.student_id AND e.subject_name=t.subject_name
+GROUP BY t.student_id,t.student_name,t.subject_name
+ORDER BY t.student_id,t.subject_name;
