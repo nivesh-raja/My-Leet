be828cb6799b1825b2afa552b9932bbf1a20fe2d
nivesh-raja <rniveshraja@gmail.com>
2026-09-17 09:40:59 +0530

    Time: 66 ms (35.32%), Space: 47.5 MB (77.74%) - LeetHub - Time: O(n), Space: O(min(n, m))

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,17 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         HashSet<Character> set = new HashSet<>();
-
-        int max=0;
-        int left=0;
-        for (int right = 0; right<s.length(); right++){
+        int max = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++){
             while (set.contains(s.charAt(right))){
                 set.remove(s.charAt(left));
                 left++;
             }
             set.add(s.charAt(right));
-            max = Math.max(max, right-left+1);
+            max = Math.max(max,right-left+1);
+            
         }
         return max;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
